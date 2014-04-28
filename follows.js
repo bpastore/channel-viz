b46f8482ba8514a33c2e9501676c0ed60d466ac1
Bruno Pastore <b.pastore@ymail.com>
2014-04-28 15:54:24 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,8 +1,8 @@
 (function ( $ ){
 
-	var defaultKey		= 'nMrWt8jqAzi76KvnVcN2MgLztJs1oZ5CmbY7Db4zvuymg16h', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [234573697], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Bruno Application', // Replaces Xively logo in the header
+	var defaultKey		= 'xJtL6YgG4Lb9yRnuTZPsJIy3XCE5ujXrGhSEQaUUgtocBOPU', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1413494697], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Bruno house', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
